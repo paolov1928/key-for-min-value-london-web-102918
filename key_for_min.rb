db77eb3f9e7f17f06320287df7e2a4c7fbf1bf0d
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-14 15:33:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+
+#MY ILLEGAL SOLUTION
 #def key_for_min_value(name_hash)
 #if name_hash.empty?
 #  nil
