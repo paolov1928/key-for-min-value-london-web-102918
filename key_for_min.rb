e6b781fb65ec634b3b458a4f5fa28eb3e713d228
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-14 15:25:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-if name_hash.empty?
-  nil
-else
-lowest = name_hash.map { |k,v| -v } .max*-1
-name_hash.index(lowest)
-end
-end
+#def key_for_min_value(name_hash)
+#if name_hash.empty?
+#  nil
+#else
+#lowest = name_hash.map { |k,v| -v } .max*-1
+#name_hash.index(lowest)
+#end
+#end
+
